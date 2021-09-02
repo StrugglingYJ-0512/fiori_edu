3e7c382473b8363d6bab4f1fd08ab3493ab46e21
YJ <lyjoo7413@naver.com>
2021-09-02 16:29:16 +0900

    Add &Modify

---
+++ zyjfio_015_0902/webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define("com.readians.yj.zyjfio0150902.ext.controller.ListReportExt",
+    [],
+    function (){
+        "use strict";
+        return {
+            action: function(oEvent) {
+                alert('action');
+            }
+        };
+});
